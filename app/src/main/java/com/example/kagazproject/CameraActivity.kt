4f1718012ebfab9635c9bf6d6dcaaeb378aa7bfc
Kalpesh Dhupia <kalpeshdhupia@gmail.com>
2021-08-01 14:39:05 +0530

    Basic Structure

---
+++ app/src/main/java/com/example/kagazproject/CameraActivity.kt
@@ -0,0 +1,63 @@
+package com.example.kagazproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_camera.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
+class CameraActivity : AppCompatActivity() {
+    var camera: Camera? = null
+    var preview: Preview? = null
+    var imageCapture: ImageCapture? = null
+    var cameraSelector: CameraSelector? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+        startCamera()
+        btnCapture.setOnClickListener {
+            takePhoto()
+        }
+    }
+
+    private fun startCamera() {
+        // to start the camera
+
+        val cameraProviderF = ProcessCameraProvider.getInstance(this)
+        cameraProviderF.addListener(Runnable {
+            val cameraProvider = cameraProviderF.get()
+            preview = Preview.Builder().build()
+            preview?.setSurfaceProvider(cameraView.createSurfaceProvider(camera?.cameraInfo))
+            imageCapture = ImageCapture.Builder().build()
+            cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            cameraProvider.unbindAll()
+            camera = cameraProvider.bindToLifecycle(this, cameraSelector!!, preview, imageCapture)
+        }, ContextCompat.getMainExecutor(this))
+
+    }
+
+    private fun takePhoto() {
+        // to capture image and save
+
+        val photoFile =
+            File(externalMediaDirs.firstOrNull(), "KamApp - ${System.currentTimeMillis()}.jpg")
+        val output = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture?.takePicture(
+            output,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    Toast.makeText(applicationContext, "img save", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    TODO("Not yet implemented")
+                }
+            })
+
+    }
+}
--- app/src/main/java/com/example/kagazproject/MainActivity.kt
+++ app/src/main/java/com/example/kagazproject/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.example.kagazproject
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PERMISSION_GRANTED //checking permission of camera is granted or not
+        ) {
+            btnTakeImage.setOnClickListener {
+                val intent = Intent(this, CameraActivity::class.java)
+                startActivity(intent)
+
+            }
+
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 0)
+        }
+
+
+
+        btnPreviewImage.setOnClickListener {
+            val intent = Intent(this, PreviewActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PERMISSION_GRANTED
+        ) {
+
+        } else {
+            Toast.makeText(this, "Please accept Camera Permission", Toast.LENGTH_SHORT)
+                .show() //asking for permission if not granted
+        }
+    }
+
+
 }
